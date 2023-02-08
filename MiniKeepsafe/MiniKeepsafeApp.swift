5a1ee54c121c8f9b98776679c8d412efc39ffa44
Peter Alt <peter@peteralt.com>
2023-02-08 14:43:50 -0700

    adds main app reducer

--- MiniKeepsafe/MiniKeepsafeApp.swift
+++ MiniKeepsafe/MiniKeepsafeApp.swift
@@ -1,7 +1,49 @@
+import ComposableArchitecture
 import SwiftUI
+
+struct MainApp: ReducerProtocol {
+    
+    struct State: Equatable {
+        var gridViewer: GridViewFeature.State = .init()
+    }
+    
+    enum Action: Equatable {
+        case appResumed
+        case appBackgrounded
+        case gridViewer(GridViewFeature.Action)
+    }
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .appResumed:
+                print("resumed")
+                return .none
+                
+            case .appBackgrounded:
+                print("backgrounded")
+                return .none
+                
+            case .gridViewer:
+                return .none
+            }
+        }
+        Scope(state: \.gridViewer, action: /Action.gridViewer) {
+            GridViewFeature()
+        }
+    }
+}
 
 @main
 struct MiniKeepsafeApp: App {
+    let store: StoreOf<MainApp> = .init(
+        initialState: .init(),
+        reducer: MainApp()
+    )
+    
+    @Environment(\.scenePhase)
+    private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             GridViewFeature_View(
@@ -10,6 +52,17 @@ struct MiniKeepsafeApp: App {
                     reducer: GridViewFeature()
                 )
             )
+            .onChange(of: scenePhase) { (newScenePhase) in
+                let viewStore = ViewStore(store)
+                switch (scenePhase, newScenePhase) {
+                case (.inactive, .active):
+                    viewStore.send(.appResumed)
+                case (.active, .inactive):
+                    viewStore.send(.appBackgrounded)
+                default:
+                    break
+                }
+            }
         }
     }
 }
