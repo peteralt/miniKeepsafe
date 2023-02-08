b87ceef45bf40840a7aeffb29c5da8108b037409
Peter Alt <peter@peteralt.com>
2023-02-08 10:03:01 -0700

    Initial Commit

---
+++ MiniKeepsafe/MiniKeepsafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniKeepsafeApp.swift
+//  MiniKeepsafe
+//
+//  Created by Peter Alt on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniKeepsafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
